aa6329bd97da2b88b71a6d7911eca2b228d4c4cf
ZinKoHtet <zinkohtet.webdev@gmail.com>
2024-11-08 19:45:55 +0630

    mui_starter

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to MUI with React!</h1>
+      <Button variant="contained" color="primary">
+        Hello, MUI!
+      </Button>
+    </div>
+  );
+}
+
+export default App;
